5d064c982fa4d8b79b55442fc5b62fc265a44c8c
Temirbaev Temirlan <temirlan213@bk.ru>
2023-12-19 21:06:51 +0600

    hotfix/nextconf

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   basePath: "",
-  webpack : (config, options) => {
+  webpack: (config, options) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
     return config;
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;

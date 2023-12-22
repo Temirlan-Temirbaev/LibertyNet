05a7bf678959882f077359607c40fd96a95e5c31
Temirlan Temirbaev <temirlan213@bk.ru>
2023-12-22 19:53:52 +0600

    feature/donate

--- shared/ui/layout/menu/Menu.tsx
+++ shared/ui/layout/menu/Menu.tsx
@@ -1,26 +1,16 @@
-import { useEffect } from "react";
-import { gsap } from "gsap";
-import { UIModal } from "../../UI-Modal";
-import { SIDEBAR_ROUTES } from "../../../constants/sidebarRoutes";
-import { Route } from "./Route";
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {UIModal} from "../../UI-Modal";
+import {SIDEBAR_ROUTES} from "../../../constants/sidebarRoutes";
+import {Route} from "./Route";
+import {ConnectWallet} from "@thirdweb-dev/react";
 
 interface MenuProps {
   isOpen: boolean;
   setIsOpen: (value: boolean) => void;
 }
 
-export const Menu = ({ isOpen, setIsOpen }: MenuProps) => {
+export const Menu = ({isOpen, setIsOpen}: MenuProps) => {
 
-  useEffect(() => {
-    gsap.to(".burger-menu", {
-      duration: 0.2,
-      ease: "power1.in",
-      right: isOpen ? "0" : "-100vw",
-    });
-  }, [isOpen]);
-
-  return <UIModal isOpen={isOpen} setIsOpen={setIsOpen}>
+  return isOpen && <UIModal isOpen={isOpen} setIsOpen={setIsOpen}>
     <div
       className={`w-[320px] 
       h-full absolute flex 
@@ -29,15 +19,18 @@ export const Menu = ({ isOpen, setIsOpen }: MenuProps) => {
       -right-full
       bg-[#121212]
       z-[3]
-      burger-menu
       px-8
+      ease-in
+      transition-all
+      delay-200
       gap-y-[30px]
+      ${isOpen ? "right-0" : "-right-full"}
       top-0`}
       onClick={event => event.stopPropagation()}>
       {SIDEBAR_ROUTES.map((route) => {
         return <Route route={route} key={`${route.label}-${route.path}`}/>;
       })}
-      <ConnectWallet className={"!flex !lg:hidden !w-full !h-[50px]"} />
+      <ConnectWallet className={"!flex !lg:hidden !w-full !h-[50px]"}/>
     </div>
   </UIModal>;
 };
--- widgets/profile/ProfileData.tsx
+++ widgets/profile/ProfileData.tsx
@@ -1,68 +1,81 @@
-import { useRouter } from "next/router";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { getProfile } from "../../shared/api/profile/getProfile";
-import { MediaRenderer, useContractWrite } from "@thirdweb-dev/react";
-import { toast } from "react-toastify";
-import { useContext } from "react";
-import { AuthContext } from "../../app/AuthProvider";
-import { UIButton } from "../../shared/ui/UI-Button";
-import { ContractContext } from "../../app/ContractProvider";
-import { banProfile } from "../../shared/api/profile/banProfile";
-import { startConversationApi } from "../../shared/api/chat/startConversation";
+import {useRouter} from "next/router";
+import {useMutation, useQuery} from "@tanstack/react-query";
+import {getProfile} from "../../shared/api/profile/getProfile";
+import {MediaRenderer, useContractWrite} from "@thirdweb-dev/react";
+import {toast} from "react-toastify";
+import {useContext, useState} from "react";
+import {AuthContext} from "../../app/AuthProvider";
+import {UIButton} from "../../shared/ui/UI-Button";
+import {ContractContext} from "../../app/ContractProvider";
+import {banProfile} from "../../shared/api/profile/banProfile";
+import {startConversationApi} from "../../shared/api/chat/startConversation";
+import {DonateModal} from "../donate/DonateModal";
 
 export const ProfileData = () => {
   const router = useRouter();
 
-  const { user } = useContext(AuthContext);
-  const { postContract } = useContext(ContractContext);
+  const {user} = useContext(AuthContext);
+  const {postContract} = useContext(ContractContext);
   const address = router.query.address;
 
-  const { data: userData } = useQuery({
+  const {data: userData} = useQuery({
     queryKey: ["getProfileByAddress", address],
     queryFn: () => getProfile(String(address)),
   });
+
+  const [isDonateModalOpen, setIsDonateModalOpen] = useState(false)
 
   const copyAddress = () => {
     navigator.clipboard.writeText(String(userData?.address));
     toast.success("Address copied");
   };
 
-  const { mutate: banTx } = useContractWrite(postContract, "switchBanned");
+  const {mutate: banTx} = useContractWrite(postContract, "switchBanned");
 
-  const { mutate: ban } = useMutation({
+  const {mutate: ban} = useMutation({
     mutationKey: ["ban"],
     mutationFn: () => {
-      banTx({ args: [userData?.address] });
+      banTx({args: [userData?.address]});
       return banProfile(String(userData?.address));
     },
     onSuccess: () => toast.success("User has been banned"),
     onError: () => toast.error("Something went wrong"),
   });
 
-  const { mutate: startConversation } = useMutation({
+  const {mutate: startConversation} = useMutation({
     mutationKey: ["startConversation", address],
     mutationFn: () => startConversationApi(String(user?.address), String(userData?.address)),
     onError: () => toast.error("Something went wrong"),
     onSuccess: (id) => router.push(`/chat?conversationId=${id}`),
   });
 
-  return <div className={"w-[300px] p-5 blur-btn bg-gray20 rounded-[20px] flex flex-col"}>
-    <MediaRenderer src={userData?.avatar} className={"w-[145px] h-[145px] rounded-xl"} />
-    <p className={"text-center text-white font-bold text-2xl mb-3"}>{userData?.nickname}</p>
-    <div onClick={copyAddress} className={"flex text-white overflow-hidden cursor-pointer mb-3"}>
-      <p>Address : <span>{userData?.address}</span></p>
-    </div>
-    {user?.address !== userData?.address && <UIButton
-      onClick={() => startConversation()}
-      className={"blur-btn bg-bluePrimary rounded-lg mb-3"}>
-      <p className={"text-white"}>Start conversation</p>
-    </UIButton>
-    }
-    {(user?.role === "moderator" && user.address !== userData?.address) && (
-      <UIButton onClick={() => ban()} className={"blur-btn bg-bluePrimary rounded-lg"}>
-        <p className={"text-white"}>Ban User</p>
+  return <>
+    <div className={"w-[300px] p-5 blur-btn bg-gray20 rounded-[20px] flex flex-col"}>
+      <MediaRenderer src={userData?.avatar} className={"w-[145px] h-[145px] rounded-xl"}/>
+      <p className={"text-center text-white font-bold text-2xl mb-3"}>{userData?.nickname}</p>
+      <div onClick={copyAddress} className={"flex text-white overflow-hidden cursor-pointer mb-3"}>
+        <p>Address : <span>{userData?.address}</span></p>
+      </div>
+      {user?.address !== userData?.address && <UIButton
+        onClick={() => startConversation()}
+        className={"blur-btn bg-bluePrimary rounded-lg mb-3"}>
+        <p className={"text-white"}>Start conversation</p>
       </UIButton>
-    )}
-  </div>
+      }
+      {user?.address !== userData?.address && <UIButton
+        onClick={() => setIsDonateModalOpen(true)}
+        className={"blur-btn bg-bluePrimary rounded-lg mb-3"}>
+        <p className={"text-white"}>Donate</p>
+      </UIButton>
+      }
+      {(user?.role === "moderator" && user.address !== userData?.address) && (
+        <UIButton onClick={() => ban()} className={"blur-btn bg-bluePrimary rounded-lg"}>
+          <p className={"text-white"}>Ban User</p>
+        </UIButton>
+      )}
+    </div>
+    <DonateModal isOpen={isDonateModalOpen} setIsOpen={setIsDonateModalOpen}
+                 receiverAddress={String(userData?.address)}/>
+  </>
     ;
 };
